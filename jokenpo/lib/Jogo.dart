aa172c23982fe7e63f34ae4df8b1f0455baac57d
luiz <glomyerjunior@hotmail.com>
2019-08-08 09:40:05 -0400

    alterações na legibilidade

--- jokenpo/lib/Jogo.dart
+++ jokenpo/lib/Jogo.dart
@@ -8,12 +8,75 @@ class Jogo extends StatefulWidget {
   _JogoState createState() => _JogoState();
 }
 
+// MÉTODOS onPressed ou onTap SÓ ACEITAM PARÂMENTROS SE O IDENTIFICADOR DA FUNÇÃO (NOME DELA) NÃO ESTIVER PRECEDIDO DE UM TIPO, NEM MESMO VOID
+
 class _JogoState extends State<Jogo> {
   @override
 
-  var _imagemPadrao = AssetImage("images/padrao.png"), texto = "", cor = Colors.black;
+  var _imagemPadrao = AssetImage("images/padrao.png"), texto = "", cor = Colors.black; // outra forma de colocar imagem
   var escolha_usuario, escolha_cpu;
   List<Escolha> possibilidades = [Escolha.Pedra, Escolha.Papel, Escolha.Tesoura];
+
+  void escolhaUsuario(Escolha e){
+    escolha_cpu = possibilidades[Random().nextInt(3)];
+    setState(() {
+      switch(e){
+        case Escolha.Pedra:
+          if(escolha_cpu == Escolha.Pedra){
+            texto = "Empate!";
+            _imagemPadrao = AssetImage("images/pedra.png");
+            cor = Colors.black;
+          }
+          if(escolha_cpu == Escolha.Papel){
+            texto = "Perdeste!";
+            _imagemPadrao = AssetImage("images/papel.png");
+            cor = Colors.red;
+          }
+          if(escolha_cpu == Escolha.Tesoura){
+            texto = "Ganhaste!";
+            _imagemPadrao = AssetImage("images/tesoura.png");
+            cor = Colors.green;
+          }
+          break;
+
+        case Escolha.Papel:
+            if(escolha_cpu == Escolha.Pedra){
+              texto = "Ganhaste!";
+              _imagemPadrao = AssetImage("images/pedra.png");
+              cor = Colors.green;
+            }
+            if(escolha_cpu == Escolha.Papel){
+              texto = "Empate!";
+              _imagemPadrao = AssetImage("images/papel.png");
+              cor = Colors.black;
+            }
+            if(escolha_cpu == Escolha.Tesoura) {
+              texto = "Perdeste!";
+              _imagemPadrao = AssetImage("images/tesoura.png");
+              cor = Colors.red;
+            }
+            break;
+
+        case Escolha.Tesoura:
+          if(escolha_cpu == Escolha.Pedra){
+              texto = "Perdeste!";
+              _imagemPadrao = AssetImage("images/pedra.png");
+              cor = Colors.red;
+          }
+          if(escolha_cpu == Escolha.Papel){
+              texto = "Ganhaste!";
+              _imagemPadrao = AssetImage("images/papel.png");
+              cor = Colors.green;
+          }
+          if(escolha_cpu == Escolha.Tesoura) {
+              texto = "Empate!";
+              _imagemPadrao = AssetImage("images/tesoura.png");
+              cor = Colors.black;
+          }
+          break;
+      }
+    });
+  }
 
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,11 +98,14 @@ class _JogoState extends State<Jogo> {
                   ),
                 ),
               ),
-              Image(image: this._imagemPadrao),
+
+              Image(image: this._imagemPadrao), // outra forma de colocar imagem
+
               Padding(
                 padding: EdgeInsets.only(bottom: 16),
                 child: Text(texto, style: TextStyle(color: cor),),
               ),
+
               Padding(
                 padding: EdgeInsets.only(top: 32, bottom: 16),
                 child: Text("Escolha uma opção abaixo",
@@ -50,83 +116,21 @@ class _JogoState extends State<Jogo> {
                   ),
                 ),
               ),
+
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
                   GestureDetector(
                     child: Image.asset("images/pedra.png", height: 100),
-                    onTap: (){
-                      setState(() {
-                        escolha_cpu = possibilidades[Random().nextInt(3)];
-                        if(escolha_cpu == Escolha.Pedra){
-                            texto = "Empate!";
-                            _imagemPadrao = AssetImage("images/pedra.png");
-                            cor = Colors.black;
-                        }
-                        if(escolha_cpu == Escolha.Papel){
-                            texto = "Perdeste!";
-                            _imagemPadrao = AssetImage("images/papel.png");
-                            cor = Colors.red;
-                        }
-                        if(escolha_cpu == Escolha.Tesoura){
-                            texto = "Ganhaste!";
-                            _imagemPadrao = AssetImage("images/tesoura.png");
-                            cor = Colors.green;
-                        }
-                      });
-                    },
+                    onTap: () => escolhaUsuario(Escolha.Pedra) // necessário ao se passar parêmetros
                   ),
                   GestureDetector(
                       child: Image.asset("images/papel.png", height: 100),
-                      onTap: (){
-                      setState(() {
-                        escolha_cpu = possibilidades[Random().nextInt(3)];
-                        if(escolha_cpu == Escolha.Pedra){
-                          texto = "Ganhaste!";
-                          _imagemPadrao = AssetImage("images/pedra.png");
-                          cor = Colors.green;
-                        }
-                        if(escolha_cpu == Escolha.Papel){
-                          texto = "Empate!";
-                          _imagemPadrao = AssetImage("images/papel.png");
-                          cor = Colors.black;
-                        }
-                        if(escolha_cpu == Escolha.Tesoura) {
-                          texto = "Perdeste!";
-                          _imagemPadrao = AssetImage("images/tesoura.png");
-                          cor = Colors.red;
-                        }
-                      });
-                    }
+                      onTap: () => escolhaUsuario(Escolha.Papel)
                   ),
                   GestureDetector(
                       child: Image.asset("images/tesoura.png", height: 100),
-                      onTap: (){
-                      setState(() {
-                        escolha_cpu = possibilidades[Random().nextInt(3)];
-                        if(escolha_cpu == Escolha.Pedra){
-                          setState(() {
-                            texto = "Perdeste!";
-                            _imagemPadrao = AssetImage("images/pedra.png");
-                            cor = Colors.red;
-                          });
-                        }
-                        if(escolha_cpu == Escolha.Papel){
-                          setState(() {
-                            texto = "Ganhaste!";
-                            _imagemPadrao = AssetImage("images/papel.png");
-                            cor = Colors.green;
-                          });
-                        }
-                        if(escolha_cpu == Escolha.Tesoura) {
-                          setState(() {
-                            texto = "Empate!";
-                            _imagemPadrao = AssetImage("images/tesoura.png");
-                            cor = Colors.black;
-                          });
-                        }
-                      });
-                    }
+                      onTap: () => escolhaUsuario(Escolha.Tesoura)
                   ),
                 ],
               )
