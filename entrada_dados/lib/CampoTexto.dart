0e29edf92399be2d89028d88c96f499447acf885
luiz <glomyerjunior@hotmail.com>
2019-08-08 11:49:52 -0400

    entrada de dados completa

--- entrada_dados/lib/CampoTexto.dart
+++ entrada_dados/lib/CampoTexto.dart
@@ -6,6 +6,9 @@ class CampoTexto extends StatefulWidget {
 }
 
 class _CampoTextoState extends State<CampoTexto> {
+
+  TextEditingController _textEditingController = TextEditingController(); // necessário para por no controller do TextField
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,28 +16,41 @@ class _CampoTextoState extends State<CampoTexto> {
         title: Text("Entrada de dados"),
       ),
 
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: TextField( // entrada de texto
-          keyboardType: TextInputType.datetime, // tipo de teclado a ser utilizado
-          obscureText: true, // esconde o texto, útil para senhas
-          decoration: InputDecoration(
-            labelText: "Digite um valor", // instrução que fica acima do campo a ser digitado
-            helperText: "Escreva acima", // instrução que fica abaixo do campo a ser digitado
-            hintText: "Escreva aqui", // instrução que fica dentro do campo a ser digitado
-          ),
-          enabled: true, // se o campo é ou não clicável para edição
-          maxLength: 10, // número máximo de caracteres
-          maxLengthEnforced: true, // desativado ele permite digitar caracteres além da quantidade estabelecida em maxLenght, só que o campo ficará vermeho, sinalizando erro
-          style: TextStyle(color: Colors.amber, fontSize: 25), // estilo do texto interno
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(32),
+            child: TextField( // entrada de texto
+              keyboardType: TextInputType.datetime, // tipo de teclado a ser utilizado
+              obscureText: true, // esconde o texto, útil para senhas
+              decoration: InputDecoration(
+                labelText: "Digite um valor", // instrução que fica acima do campo a ser digitado
+                helperText: "Escreva acima", // instrução que fica abaixo do campo a ser digitado
+                hintText: "Escreva aqui", // instrução que fica dentro do campo a ser digitado
+              ),
+              enabled: true, // se o campo é ou não clicável para edição
+              maxLength: 10, // número máximo de caracteres
+              maxLengthEnforced: true, // desativado ele permite digitar caracteres além da quantidade estabelecida em maxLenght, só que o campo ficará vermeho, sinalizando erro
+              style: TextStyle(color: Colors.amber, fontSize: 25), // estilo do texto interno
 
-          onChanged: (String s){ // função anônica que captura o que é digitado
-            print("Valor digitado: " + s);
-          },
-          onSubmitted: (String s){ // função anônica que captura o que é digitado
-            print("Valor confirmado: " + s);
-          },
-        ),
+              onChanged: (String s){ // função anônica que captura o que é digitado
+                print("Valor digitado: " + s);
+              },
+              onSubmitted: (String s){ // função anônica que captura o que é digitado
+                print("Valor confirmado: " + s);
+              },
+              controller: _textEditingController, // serve para recuperar o texto em situações onde não estejamos trabalhando com os métodos onChanged, on Submited, etc
+            ),
+          ),
+
+          RaisedButton(
+            child: Text("Salvar"),
+            color: Colors.lightGreen,
+            onPressed: (){
+              print("Texto recuperado: " + _textEditingController.text);
+            },
+          )
+        ]
       ),
 
       bottomNavigationBar: Container(
