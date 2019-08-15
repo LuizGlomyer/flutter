b12ab1455c5ed59a0c0cb6a5d475a5f15ec2c3e0
luiz <glomyerjunior@hotmail.com>
2019-08-15 08:47:47 -0400

    app preço do bitcoin terminado

--- preco_bitcoin/lib/Home.dart
+++ preco_bitcoin/lib/Home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+import 'dart:async';
+import 'dart:convert';
+
 
 class Home extends StatefulWidget {
   @override
@@ -7,32 +11,56 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  String _valorBitcoin = "Clique abaixo";
+
   @override
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.all(32),
       color: Colors.white,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
           Image.asset("img/bitcoin.png"),
           
           Padding(
-            padding: EdgeInsets.only(top: 15, bottom: 15),
+            padding: EdgeInsets.only(top: 30, bottom: 10),
             child: Text(
-              "Clique para verificar o valor",
+              _valorBitcoin,
               style: TextStyle(
-                fontSize: 32
+                fontSize: 25,
+                color: Colors.black54,
+                decoration: TextDecoration.none
               ),
             ),
           ),
           
           RaisedButton(
             color: Colors.orange,
-            child: Text("Atualizar"),
+            child: Text(
+                "Atualizar",
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+            onPressed: _checarValor,
           )
           
         ],
       ),
     );
   }
+
+  void _checarValor() async{
+    String url = "https://blockchain.info/ticker";
+    http.Response resposta = await http.get(url); // await faz a atribuição aguardar a requisição
+    Map<String, dynamic> retorno = json.decode(resposta.body);
+    print(retorno["BRL"]);
+
+    setState(() {
+      _valorBitcoin = "R\$ ${retorno["BRL"]["buy"]}";
+    });
+
+  }
 }
+
+
--- preco_bitcoin/lib/main.dart
+++ preco_bitcoin/lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:preco_bitcoin/Home.dart';
 
+import 'package:flutter/services.dart'; // para tirar a status bar e a barra de navegação
 
-void main() => runApp(
-  MaterialApp(
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  )
-);
+void main() {
+    SystemChrome.setEnabledSystemUIOverlays([]); // retira as áreas
+    //SystemChrome.setEnabledSystemUIOverlays(SystemUiOverlay.values); // as mostra novamente
+
+    runApp(
+      MaterialApp(
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      )
+    );
+}
