395e42a889d1332f8a6422bc04af42832da3e254
luiz <glomyerjunior@hotmail.com>
2019-09-09 11:24:49 -0400

    começando execução de vídeos

--- aprenda_ingles/lib/TabBichos.dart
+++ aprenda_ingles/lib/TabBichos.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:audioplayers/audio_cache.dart';
 
 class TabBichos extends StatefulWidget {
   @override
@@ -7,8 +7,70 @@ class TabBichos extends StatefulWidget {
 }
 
 class _TabBichosState extends State<TabBichos> {
+
+  AudioCache _audioCache = AudioCache(prefix: "audio/");
+
+  _executar(String nomeAudio) async {
+    await _audioCache.play(nomeAudio + ".mp3");
+  }
+
+  @override
+  void initState() {
+    super.initState(); // como a execução é feita em cache, primeiro os arquivos são copiados para a memória e só depois são executados, gerando um delay
+    _audioCache.loadAll([ // este método pré carrega os arquivos na memória
+      "cao.mp3", "gato.mp3", "leao.mp3",
+      "macaco.mp3", "ovelha.mp3", "vaca.mp3"
+    ]);
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Text("a");
+    double largura = MediaQuery.of(context).size.width;
+    double altura = MediaQuery.of(context).size.height;
+    //MediaQuery.of(context).size.aspectRatio
+
+    return GridView.count(
+      crossAxisCount: 2, // número de colunas que desejamos criar
+      childAspectRatio: largura/altura * 2, // define como o conteúdo irá se acomodar dentro da tela
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+            _executar("cao");
+          },
+          child: Image.asset("assets/img/cao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("gato");
+          },
+          child: Image.asset("assets/img/gato.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("leao");
+          },
+          child: Image.asset("assets/img/leao.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("macaco");
+          },
+          child: Image.asset("assets/img/macaco.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("ovelha");
+          },
+          child: Image.asset("assets/img/ovelha.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("vaca");
+          },
+          child: Image.asset("assets/img/vaca.png"),
+        ),
+      ],
+    );
   }
 }
--- aprenda_ingles/lib/TabNumeros.dart
+++ aprenda_ingles/lib/TabNumeros.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:audioplayers/audio_cache.dart';
 
 class TabNumeros extends StatefulWidget {
   @override
@@ -7,8 +7,65 @@ class TabNumeros extends StatefulWidget {
 }
 
 class _TabNumerosState extends State<TabNumeros> {
+
+  AudioCache _audioCache = AudioCache(prefix: "audio/");
+
+  _executar(String nomeAudio){
+    _audioCache.play(nomeAudio + ".mp3");
+  }
+
+  @override
+  void initState() {
+    super.initState(); // como a execução é feita em cache, primeiro os arquivos são copiados para a memória e só depois são executados, gerando um delay
+    _audioCache.loadAll([ // este método pré carrega os arquivos na memória
+      "1.mp3", "2.mp3", "3.mp3",
+      "4.mp3", "5.mp3", "6.mp3"
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text("b");
+    return GridView.count(
+      crossAxisCount: 2, // número de colunas que desejamos criar
+      childAspectRatio: MediaQuery.of(context).size.aspectRatio * 2, // define como o conteúdo irá se acomodar dentro da tela
+      children: <Widget>[
+        GestureDetector(
+          onTap: (){
+            _executar("1");
+          },
+          child: Image.asset("assets/img/1.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("2");
+          },
+          child: Image.asset("assets/img/2.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("3");
+          },
+          child: Image.asset("assets/img/3.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("4");
+          },
+          child: Image.asset("assets/img/4.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("5");
+          },
+          child: Image.asset("assets/img/5.png"),
+        ),
+        GestureDetector(
+          onTap: (){
+            _executar("6");
+          },
+          child: Image.asset("assets/img/6.png"),
+        ),
+      ],
+    );
   }
 }
