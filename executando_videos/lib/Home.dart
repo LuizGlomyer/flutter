a74929024f04b0fa4c3b57be85f7e92415ee7df8
luiz <glomyerjunior@hotmail.com>
2019-09-10 08:43:03 -0400

    terminando execução de videos

--- executando_videos/lib/Home.dart
+++ executando_videos/lib/Home.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,8 +9,46 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset("videos/exemplo.mp4")
+      ..setLooping(true)
+      ..initialize().then((_) {
+        // passar um parâmetro é obrigatório, mas usando _ passamos um argumento vazio o qual não aloca memória
+        setState(() {
+          // precisamos do set state para mostrar o primeiro frame do vídeo antes mesmo dele ser executado
+          //_controller.play();
+        });
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: _controller.value.initialized
+            ? AspectRatio(
+                // define uma área com base no aspect ratio do dispositivo
+                aspectRatio: _controller.value.aspectRatio,
+                child: VideoPlayer(_controller),
+              )
+            : Text("Pressione play"),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        child: Icon(
+          _controller.value.isPlaying ? Icons.pause : Icons.play_arrow
+        ),
+        onPressed: (){
+          setState(() {
+            _controller.value.isPlaying ? _controller.pause(): _controller.play() ;
+          });
+        },
+      ),
+
+    );
   }
 }
